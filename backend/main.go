566689f260f6abaa0a71d57351c2c8cd203a3931
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-08-31 19:36:35 +0200

    added redis client; ping redis via endpoint /redis

--- backend/main.go
+++ backend/main.go
@@ -1,12 +1,22 @@
 package main
 
 import (
+	"fmt"
 	"net/http"
 	"os"
+
+	"github.com/go-redis/redis"
 )
+
+var client = redis.NewClient(&redis.Options{
+	Addr:     "localhost:6379", // TODO: use environment variable from Docker
+	Password: "",               // NOTE: no password is used currently
+	DB:       0,                // NOTE: this is the default DB
+})
 
 func main() {
 	http.HandleFunc("/hello", hello)
+	http.HandleFunc("/redis", redisCanary)
 	port := os.Getenv("PORT")
 	if port == "" {
 		port = "8000"
@@ -16,4 +26,13 @@ func main() {
 
 func hello(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello, World!"))
+}
+
+func redisCanary(w http.ResponseWriter, r *http.Request) {
+	pong, err := client.Ping().Result()
+	if err != nil {
+		message := fmt.Sprintf("ping redis: %v", err)
+		http.Error(w, message, http.StatusInternalServerError)
+	}
+	w.Write([]byte(pong))
 }
