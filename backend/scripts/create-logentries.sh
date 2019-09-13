c76633ddf7a4f650c9b7a418e45d1e602a67bb39
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-09-13 10:19:33 +0200

    Backend: more test data

--- backend/scripts/create-logentries.sh
+++ backend/scripts/create-logentries.sh
@@ -2,6 +2,7 @@
 
 username='paedu'
 date='2019-09-01'
-tag='TooLittleSleep'
 
-curl -X PUT localhost:8000/${username}/logentry/${date}/${tag}
+curl -X PUT localhost:8000/${username}/logentry/${date}/TooLittleSleep
+curl -X PUT localhost:8000/${username}/logentry/${date}/SmokedCigarettes
+curl -X PUT localhost:8000/${username}/logentry/${date}/DrankBeerYesterday
