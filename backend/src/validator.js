d8e7ac000a56bd5d1bae8bd6935f43cd25e7354f
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-09-13 14:59:12 +0200

    Backend: date validator function and test

--- backend/src/validator.js
+++ backend/src/validator.js
@@ -1,5 +1,20 @@
-function validDate(date) {
-    return false;
+function validDate(dateStr) {
+    const parts = dateStr.split('-');
+    if (parts.length !== 3) {
+        return false;
+    }
+    const [year, month, day] = parts;
+    if (year.length !== 4 || month.length !== 2 || day.length !== 2) {
+        return false;
+    }
+    if (month < 1 || month > 12) {
+        return false;
+    }
+    if (day < 0 || day > 31) {
+        return false;
+    }
+    let date = new Date(year, month - 1, day);
+    return date.getFullYear() !== NaN && date.getMonth() !== NaN && date.getDay() !== NaN;
 }
 
 function validUsername(username) {
--- backend/src/validator.test.js
+++ backend/src/validator.test.js
@@ -1,5 +1,14 @@
 const [validDate, validUsername, validTag] = require('./validator');
 
-test('"foobar" is not a valid date', () => {
-    expect(validDate('foobar')).toBe(false);
+test('the date format must be YYYY-MM-DD', () => {
+    const tests = new Map();
+    expect(validDate('2019-09-13')).toBe(true);
+    expect(validDate('2019-01-01')).toBe(true);
+    expect(validDate('2019-12-31')).toBe(true);
+    expect(validDate('2019-31-12')).toBe(false);
+    expect(validDate('19-01-01')).toBe(false);
+    expect(validDate('1900-06-24')).toBe(true);
+    expect(validDate('1999-02-28')).toBe(true);
+    expect(validDate('1999-2-28')).toBe(false);
+    expect(validDate('2019-09-13')).toBe(true);
 });
