1dd29631d6b6d13e9eb8ff1d2bedaeea87ef6ceb
Patrick Bucher <patrick.bucher@stud.hslu.ch>
2019-09-28 13:37:32 +0200

    Backend: Credentials vereinfacht

--- backend/src/credentials.js
+++ backend/src/credentials.js
@@ -1,7 +1,7 @@
 // NOTE: these credentials are for demo purposes only
 const credentials = new Map([
-    ['johndoe', 'topsecret1337'],
-    ['toni', 'Geheim1!']
+    ['johndoe', 'topsecret'],
+    ['toni', 'geheim']
 ]);
 
 const secretKey = 'bohBahqu5iH7ooz8Chietiemooteekae';
